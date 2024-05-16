f8ed51e539ab4afb1625cf67207e989fdea84d57
”ukrWily” <ukrwily@gmail.com>
2024-05-16 22:20:46 +0300

    PC second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+const Header = () => {
+  return <h2>Hello World!</h2>;
+};
+
+const Field = () => {
+  return <input type="text" placeholder="Type here" />;
+};
+
+function Btn() {
+  // const text = "Log in";
+  const res = () => {
+    return "Log in";
+  };
+  return <button>{res()}</button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
