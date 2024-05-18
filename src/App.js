a9e4c29a6b4469168b8a80b70d840e6df39d3ad8
ukrWily <ukrwily@gmail.com>
2024-05-18 13:46:43 +0300

    L props

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
-// import logo from "./logo.svg";
-import { Component } from "react";
 import "./App.css";
 
-const Header = () => {
-  return <h2>Hello World!</h2>;
-};
-
-// const Field = () => {
-//   const holder = "Type here";
-//   const styledField = {
-//     width: "300px",
-//   };
-//   return <input type="text" placeholder={holder} style={styledField} />;
-// };
-
-class Field extends Component {
-  render() {
-    const holder = "Type here";
-    const styledField = {
-      width: "300px",
-    };
-
-    return <input type="text" placeholder={holder} style={styledField} />;
-  }
-}
-
-function Btn() {
-  const text = "Log in";
-  const logged = true;
-
-  return <button>{logged ? "Enter" : text}</button>;
+function WhoAmI({ name, surname, link }) {
+  return (
+    <div className="">
+      <h1>
+        My name is {name}, surname - {surname}
+      </h1>
+      <a href={link}>My profile</a>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Field />
-      <Btn />
+      <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Mark" surname="Donovan" link="twitter.com" />
     </div>
   );
 }
 
-export { Header };
 export default App;
