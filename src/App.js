483c4096bd8b3577318b43bc5fad8bdeb87b01a0
”ukrWily” <ukrwily@gmail.com>
2024-05-19 20:47:00 +0300

    PC change state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import { Component } from "react";
+
 import "./App.css";
 
-function WhoAmI({ name, surname, link }) {
-  return (
-    <div className="">
-      <h1>
-        My name is {name}, surname - {surname}
-      </h1>
-      <a href={link}>My profile</a>
-    </div>
-  );
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: "+++",
+    };
+  }
+
+  nextYear = () => {
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div className="">
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>
+          My name is {name}, surname - {surname}, age - {this.state.years} years
+        </h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
 }
 
 function App() {
@@ -21,3 +41,5 @@ function App() {
 }
 
 export default App;
+
+// ({ name, surname, link })
